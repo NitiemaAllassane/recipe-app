af7008c5fc970717347bfa4d2c7781f1ee2c1cb3
Nitiema Allassane <allassanenitiema@gmail.com>
2026-01-19 20:41:43 +0000

    Ajout d'un grand titre

--- index.php
+++ index.php
@@ -33,9 +33,11 @@ $recipes = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Affichage des listes des recettes</title>
 </head>
 <body>
+    <h1>Appli de recettes</h1>
     <h2>Listes des recettes</h2>
     <ul>
         <?php for ($line = 0; $line <= 3; $line++):  ?>
